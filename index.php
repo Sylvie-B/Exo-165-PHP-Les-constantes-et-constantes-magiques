e3d4a433737fbddfbbe7c5dbcdeb542a4a5baec5
Sylvie-B <unsolo@hotmail.fr>
2021-02-01 09:37:07 +0100

    const et server

--- index.php
+++ index.php
@@ -9,9 +9,16 @@
  *    longueur de la chaîne de la troisième constante ( attention aux priorités )
  */
 // TODO Votre code ici.
-
+$_a = 12;
+$_b = 2;
+echo $_a * $_b."<br>";
+$_Cool = "C'est cool PHP";
+echo ($_a + $_b) * strlen($_Cool);
 
 /**
  * 6. Utilisez la bonne constante magique et éventuellement la bonne fonction pour afficher un maximum d'informations sur la page actuelle4
  */
 // TODO Votre code ici.
+foreach ($_SERVER as $key => $value){
+    echo "<br>".$key." => ".$value."<br>";
+}
